82e835e4bb3541720e15bb8273d881ae6836d9df
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-05-20 23:07:09 +0200

    Exercise-2-Complted
    added heading component,
    Test: The header should be displayed with a logo on the page.= True;

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        alt="CYF-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+    </div>
+  );
+};
+export default Heading;
