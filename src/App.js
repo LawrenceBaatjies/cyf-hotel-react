9788c9ef3021f76948b01cff4ac2bd9edf6a5560
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-05-21 19:42:39 +0200

    Exercise-3Completed
    Create TouristInfoCards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
-
-import Bookings from "./Bookings";
-import "./App.css";
 import Heading from "./Heading";
+import Bookings from "./Bookings";
+import Restaurant from "./Restaurant";
+import TouristInfoCards from "./TouristInfoCards";
+// import Footer from "./Footer";
+
+import "./Style.css";
 
 const App = () => {
+  const footerData = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
-      <Bookings />
       <Heading />
+      <Bookings />
+      <Restaurant />
+      <hr />
+      <TouristInfoCards />
+      <hr />
+      {/* <Footer footerData={footerData} /> */}
     </div>
   );
 };
