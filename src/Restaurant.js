4e957d92aa337c6d3e36419d3db961e7d53b7d62
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-05-21 20:55:26 +0200

    Exercise-8- Completed
    Render the Restaurant component.

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Order from "./Order";
 
-const Restaurant = () => {
-  const pizzas = 0;
-  return (
-    <div>
-      <h3>Restaurant Orders</h3>
-      <ul>
-        <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class Restaurant extends React.Component {
+  render() {
+    return (
+      <div className="container d-flex justify-content-center">
+        <div className="card bg-light text-dark">
+          <div className="card-body">
+            <h3>Restaurant Orders</h3>
+            <ul className="list-group list-group-flush">
+              <Order orderType={"Pizzas"} />
+              <Order orderType={"Salads"} />
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Restaurant;
